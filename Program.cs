b56bc44341136a9c514aeca4dd5287ee3e3c5c4b
Telli <tellihamzayeva@gmail.com>
2023-12-18 03:18:18 +0400

    1 issue

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// Квадрат в ОБЕ стороны.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+// Console.WriteLine("input 2 numbers: ");
+        
+// // Считываем ввод пользователя и преобразуем его в числа
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number1==number2*number2 || number2==number1*number1)
+// {
+//     Console.WriteLine($"{number1} is square of the {number2} or {number2} is square of the {number1}");
+// }
+// else 
+// {
+//     Console.WriteLine($"neitner {number1} nor {number2} is squere of each other");
+// }
+
